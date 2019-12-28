6c666b1f43067daf9febc51c6c17ccb8112a282f
Mamadou Cisse <mciissee@gmail.com>
2019-12-28 19:55:09 +0100

    fix bugs on LambdaMethodVisitor class

--- src/main/java/fr/umlv/Retro/App.java
+++ src/main/java/fr/umlv/Retro/App.java
@@ -15,14 +15,14 @@ import fr.umlv.Retro.models.TransformOptions;;
 public class App {
     public static void main(String[] args) throws IOException {
 
-    	var cr = new ClassReader(new FileInputStream("../Drafts/TestLambda.class"));
-    	//var cr = new ClassReader("TestConcat");
+    	//var cr = new ClassReader(new FileInputStream("../Drafts/TestLambda.class"));
+    	var cr = new ClassReader("TestLambda");
         var cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
         var options = TransformOptions.fromCommandLine(args);
         cr.accept(new ClassTransformer(cw, options), 0);
  
         if (Files.isDirectory(Paths.get("../Drafts/"))) {
-        	var fos = new FileOutputStream("../Drafts/TestConcat.class");
+        	var fos = new FileOutputStream("../Drafts/TestLambda.class");
         	fos.write(cw.toByteArray());
         	fos.close();
         }
