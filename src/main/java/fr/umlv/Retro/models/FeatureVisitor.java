69e6a1ecf357f1f2b0f4c80b11eba685da1705ef
stephane <stephane.sanches.fernandes@gmail.com>
2020-02-02 22:29:43 +0100

    add doc

--- src/main/java/fr/umlv/Retro/models/FeatureVisitor.java
+++ src/main/java/fr/umlv/Retro/models/FeatureVisitor.java
@@ -5,6 +5,17 @@ import org.objectweb.asm.ClassVisitor;
 import fr.umlv.retro.ClassTransformer;
 
 
+/**
+ * Interface implemented by a ClassVisitor able to detect and rewrite a feature.
+ */
 public interface FeatureVisitor {
+
+	/**
+	 * 
+	 * @param ci information about the visited class.
+	 * @param cv the class visitor to which the calls of visitMethods should be delegated.
+	 * @param tr current class transformer.
+	 * @return The ClassVisitor itself.
+	 */
 	ClassVisitor visit(ClassInfo ci, ClassVisitor cv, ClassTransformer tr);
 }
